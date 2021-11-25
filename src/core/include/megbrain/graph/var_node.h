e220e2ebf936758e75854d69d7a3439f5a72c461
Megvii Engine Team <megengine@megvii.com>
2021-11-25 13:41:11 +0800

    Revert "fix(mge): fix shape infer's condition in lite"
    This reverts commit a8f73194765d15cfd564030c5063b90e63abfe9c.

--- src/core/include/megbrain/graph/var_node.h
+++ src/core/include/megbrain/graph/var_node.h
@@ -489,15 +489,11 @@ public:
     MemAllocPlan& init_mem_plan(const DeviceTensorND* fixed_alloc = nullptr);
 
     /*!
-     * \brief check infer shape capablity by check m_static_infer_trait's shape infer
+     * \brief get the shape and value infer trait
      */
-    bool capable_shape_infer();
-
-    /*!
-     * \brief check infer shape capablity by check m_static_infer_trait's value infer
-     */
-    bool capable_value_infer();
-
+    const std::tuple<void*, void*>& get_static_infer_trait() {
+        return m_static_infer_trait;
+    }
 
 private:
     //! whether its memory should be allocated by mgb system during graph
